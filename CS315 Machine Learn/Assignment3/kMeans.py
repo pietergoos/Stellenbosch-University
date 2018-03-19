82a95608d25de6d76971dd02850684e3e85f9d76
Pieter Goos <19231466@sun.ac.za>
2018-03-19 22:57:13 +0200

    k Means classifier created

---
+++ CS315 Machine Learn/Assignment3/kMeans.py
@@ -0,0 +1,49 @@
+import numpy as np
+import scipy as sp
+
+##############################################################
+## THIS CLASS CALCULATES THE LOGISTIC REGRESSION PREDICTION ##
+##############################################################
+## This file is largely based on LR_b.py in the same folder ##
+##############################################################
+
+class myKM():
+    def __init__(self):
+        pass
+
+    def train(self, X, nClasses, dim = 2):
+        classes = []
+        finished = False
+
+        for i in range(nClasses):
+            classes.append(i)
+
+        self.classes = classes
+        rMeans = np.random.rand(nClasses, dim) #each label gets components for each dimension
+
+        while not finished:
+            uN = self.refresh(X, rMeans, classes)
+            finished = self.isClose(rMeans, uN, 0.0)
+            rMeans = uN
+
+    def isClose(self, wOld, wNew, threshold):
+        return np.any(np.abs(np.array(wOld) - np.array(wNew))) <= threshold
+
+    def refresh(self, X, uOld, classes):
+        y = []
+        Xo = []
+        for i in range(len(X)):
+            dis = []
+            for j in range(len(classes)): #This should be the number of labels
+                dis.append(np.linalg.norm(X[i] - uOld[j]))
+            maxi = np.argmin(np.array(dis))
+            y.append(classes[maxi])
+        y = np.array(y)
+        u = []
+        for i in range(len(classes)):
+            Xo.append(X[y==i, :])
+            u.append(np.mean(a=Xo[i], axis = 0))
+        u= np.array(u)
+        self.y = y
+        self.u = u
+        return u
